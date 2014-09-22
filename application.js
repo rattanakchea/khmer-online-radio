089d5919a64fb49da772ab5410253048bd6f6649
rattanakchea <rattanak.chea@gmail.com>
2014-09-22 14:35:18 +0700

    add gitignore, v1.1 *new - ajax loading - remove not working channels - add new channels

--- application.js
+++ application.js
@@ -1,34 +1,63 @@
 var app = {
     //variables
-
+     //song : $('audio').get(0),
+    loadeddata: function() {
+        console.log("can play");
+        $("#loading").fadeOut();
+    },
+    onerror: function() {
+        console.log("cannot play..error");
+        //$('audio').get(0).src = "http://112.121.150.133:9308/;stream.nsv";
+        $('#panel').text("Error with this channel..");
+        $("#loading").fadeOut(1000);
+    },
+    onwaiting: function() {
+        console.log("waiting...");
+        $("#loading").fadeIn();
+    },
     init: function() {
         //load in our radio list
         $.get("radioStations.json", {}, function(res, code) {
             var s = "";
             for (var i = 0; i < res.length; i++) {
+                var src = res[0].url;
                 s += "<li class='radio' data-url='" + res[i].url + "'><a href='#'>";
                 s += "<img src = '" + res[i].image + "' />";
                 s += "<h2>" + res[i].name + "</h2>";
                 s += "<p>" + res[i].description + "</p>";
                 s += "</a></li>";
             }
-
+            //$('audio').get(0).src = src;
             $('#radioList').html(s).listview("refresh");
         }, "json");
     },
     play: function() {
-        var song = $('audio').get(0);
+
         //play proper radio channel on click
         $('#radioList').on('click', 'li', function() {
+            var song = $('audio').get(0);
 
             var url = $(this).data('url');
-            
+
             //the same channel
             if (url == song.src) {
-                song.pause();
+                if (song.paused) {
+                    song.play();
+                    //console.log($(this));
+                    //var panel = $("playing " + $(this).find('h2').text());
+                    //$('div#audio').append(panel);
+
+                } else {
+                    song.pause();
+                }
             } else {
                 song.src = url;
                 song.play();
+                    var panel = $('span#panel').text("");
+                    var text = ("playing " + $(this).find('h2').text());
+                    console.log(text);
+                    panel.text(text);
+                
             }
 
 
@@ -39,6 +68,7 @@ var app = {
 
 $(document).ready(function()
 {
+
     app.init();
     app.play();
 
